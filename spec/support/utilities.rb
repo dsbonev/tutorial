9af0a5f9bd7a51df2733b600a706d7fc90c823bc
Dimitar Bonev <dsbonev@gmail.com>
2014-10-29 12:44:23 +0200

    add rspec test helpers to imrpove specs

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,13 @@
 include ApplicationHelper
+
+def signin_user(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    find('div.alert.alert-error').text.should eq message
+  end
+end
